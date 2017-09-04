3bb38fd46d2f48578ef4cf14efbeb7a7cf663194
Iris <hsiaohan.huang@ryerson.ca>
2017-09-04 16:11:48 -0400

    Python NLP: importing data, formatting, basic cleaning, exploring

--- Iris_Python/Iris_NLPPython.py
+++ Iris_Python/Iris_NLPPython.py
@@ -2,13 +2,71 @@ import os
 import regex as re
 import numpy as np
 import pandas as pd
-import matplotlib as plt
+import matplotlib.pyplot as plt
 
+#-----importing files----#
 scriptdir = os.path.dirname(os.path.abspath(__file__))
-sp_file = os.path.join(scriptdir, "trainingtext.txt")
-with open(sp_file, 'r', encoding='utf8') as file:
-    print(file.readline())
+train_txt = os.path.join(scriptdir, "trainingtext.txt")
+with open(train_txt, 'r', encoding='utf8') as file:
     train = file.read()
+scriptdir2 = os.path.dirname(os.path.abspath(__file__))
+train_csv = os.path.join(scriptdir2, "training_variants.csv")
+train_var = pd.read_csv(train_csv, header=0, index_col=0)
+#print(len(train_var)) #3321
 
-pattern = '^%^'
-print(re.findall(pattern, train))
+#----turning training text file into dataframe----#
+train_split_line = re.split('\n+', train)
+train_split_delim = [re.split(r"\^\%\^", entry, 2) for entry in train_split_line]
+train_split_new = train_split_delim[1:]
+train_ind = [item[0] for item in train_split_new]
+train_text = [text[-1] for text in train_split_new]
+train_text_new = train_text[:-1]
+train_mer = train_var.copy() #merging var and text files together
+train_mer['Text'] = train_text_new
+#print(train_mer.shape) #(3321, 4)
+#print(train_mer.info())
+
+#----basic cleaning----#
+
+##remove null values
+null_mask = train_mer['Text'].str.contains('^null')
+contain_null = train_mer['Text'].loc[null_mask]
+#print(contain_null) #consistent with before: 1109, 1277, 1407, 1639, 2755; 448 and 1385 have articles behind "null"
+null_ind = 1109, 1277, 1407, 1639, 2755
+trainmer_new = train_mer.drop(train_mer.index[[null_ind]])
+#print(trainmer_new.info()) #cleaning "null" entries: (3316, 4) after the five null entries are deleted
+trainmer_new.loc[[448, 1385], 'Text'] = trainmer_new['Text'].loc[[448, 1385]].str.replace('null', ' ')
+#print(trainmer_new.iloc[[448, 1385]]) #replaced the "null" in front of text with empty string
+
+##convert gene and class into category type
+trainmer_new.Gene = trainmer_new.Gene.astype('category')
+trainmer_new.Class = trainmer_new.Class.astype('category')
+#print(trainmer_new.iloc[[448, 1385]])
+#print(trainmer_new.info())
+
+#----exploratory analysis----#
+
+##Class frequency
+class_freq = trainmer_new.groupby('Class')['Gene'].count()
+class_freq.plot(kind='bar', use_index=True, title='Class frequency')
+plt.xlabel('Class')
+plt.xticklabels(rotation=0)
+plt.ylabel('Frequency')
+plt.show()
+
+#Text frequency
+text_freq = trainmer_new.groupby('Text')['Class'].count()
+#print(text_freq.sort_values(ascending=False))
+
+#Gene frequency
+gene_freq = trainmer_new.groupby('Gene')['Text'].count()
+#print(gene_freq.sort_values(ascending=False))
+
+var_freq = trainmer_new.groupby('Variation')['Text'].count()
+#print(var_freq.sort_values(ascending=False))
+
+#print(trainmer_new[['Gene', 'Variation']].drop_duplicates()) #check if gene-var pairs are unique
+trainmer_multiind = trainmer_new.set_index(['Gene', 'Variation']) #indexed by unique pairs of gene and variation
+#print(trainmer_multiind.info()) #memory usage reduced from 256KB+ to 77KB+
+
+#----preprocessing of text files----#
